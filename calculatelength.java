b9cbccd08014a8462797b3e2775c2b1160363c92
dokeshraddha <shraddhadoke1999@gmail.com>
2021-07-28 19:06:08 +0530

    Welcome to Line Comparison Computation Program

---
+++ calculatelength.java
@@ -0,0 +1,13 @@
+public class calculatelength {
+   public static void main(String[] args) {
+      int x1=5;
+      int x2= 10;
+      int y1= 15;
+      int y2= 15;
+      int calculatelength;
+      calculatelength = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+      System.out.println(Math.sqrt(calculatelength));
+
+   }
+
+}
